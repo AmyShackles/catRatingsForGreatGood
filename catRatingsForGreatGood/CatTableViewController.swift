9baf3a49d7d7176cec74a5de8a6c1653a1f0423c
Amy Shackles <amyshackles@gmail.com>
2020-02-23 17:29:59 -0800

    Cats can now be deleted from list

--- catRatingsForGreatGood/CatTableViewController.swift
+++ catRatingsForGreatGood/CatTableViewController.swift
@@ -17,6 +17,9 @@ class CatTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
         // Load the sample data.
         loadSampleCats()
     }
@@ -53,25 +56,23 @@ class CatTableViewController: UITableViewController {
     }
 
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            cats.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
